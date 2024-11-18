fc7758cc135e16ded6c0cadf46861f175a7f5c2d
Ankushvashisth <151016712+Ankushvashisth@users.noreply.github.com>
2024-11-18 18:37:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const slider = document.querySelector('.slider');
 
 function activate(e) {
   const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0]);
-  e.target.matches('.prev') && slider.append(items[items.ngth -1]);
+  // Check if the clicked element is the 'next' or 'prev' button
+  if (e.target.matches('.next')) {
+    slider.append(items[0]); // Move the first item to the end
+  } 
+  else if (e.target.matches('.prev')) {
+    slider.prepend(items[items.length - 1]); // Move the last item to the beginning
+  }
 }
-document.addEventListener('click',activate,false);
+
+// Attach the event listener to the document
+document.addEventListener('click', activate, false);
